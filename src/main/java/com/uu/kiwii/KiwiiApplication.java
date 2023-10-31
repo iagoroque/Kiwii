77c50dc5e998e1da0240e61de11d7303a4391dc9
Iago Roque <71741741+iagoroque@users.noreply.github.com>
2023-10-31 15:54:10 -0300

    First commit

---
+++ src/main/java/com/uu/kiwii/KiwiiApplication.java
@@ -0,0 +1,13 @@
+package com.uu.kiwii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiwiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiwiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uu/kiwii/KiwiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uu.kiwii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiwiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
