3cf381c296a382a08e0960efc17882c4cc7df576
Harrison <harrisonborges13@gmail.com>
2023-10-31 18:44:55 -0300

    fixed bug

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
 INSERT INTO subjects (course_id, name, links) VALUES 
-(1, 'LPA', '{"google.com", "youtube.com"}');
+(1, 'LPA', '{"orkut.com", "google.com"}');
+(2, 'POO', '{"youtube.com", "facebook.com"}');
 
 INSERT INTO courses (name, course_rm) VALUES 
 ('Bacharelado em Engenharia de Software', '{"1","2","3","4","5"}');
