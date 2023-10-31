aa79e6c9c76b010a582bf3e61fdff41cd1f181d6
Oseias Lopes <83646294+OseiasYC@users.noreply.github.com>
2023-10-31 19:03:51 -0300

    Finish database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,11 @@
-INSERT INTO subjects (course_id, name, links) VALUES 
-(1, 'LPA', '{"google.com", "youtube.com"}');
+-- COURSES:
+    INSERT INTO courses ( name, rm) VALUES 
+    ('Bacharelado em Engenharia de Software', '{"200019492","200019213","200016717"}'),
+    ('Análise e Desenvolvimento de Sistemas', '{"200019333","200019321","200016123"}');
 
-INSERT INTO courses (name, course_rm) VALUES 
-('Bacharelado em Engenharia de Software', '{"1","2","3","4","5"}');
+-- SUBJECTS:
+    INSERT INTO subjects (course_id, name, links) VALUES 
+    (1, 'Gestão do Conhecimento', '{"1Waq2GevVK6fM4sTkMOzkKKh32Tme-LgN", "1vgzrnRUITFp_YYDztSlSgAioqRlSvUZ-"}'),
+    (1, 'Programação Orientada a Objetos', '{"1Waq2GevVK6fM4sTkMOzkKKh32Tme-LgN", "1vgzrnRUITFp_YYDztSlSgAioqRlSvUZ-"}'),
+    (2, 'Programação Orientada a Objetos', '{"1Waq2GevVK6fM4sTkMOzkKKh32Tme-LgN", "1vgzrnRUITFp_YYDztSlSgAioqRlSvUZ-"}'),
+    (2, 'Lógica de Programação e Algoritmos', '{"1Waq2GevVK6fM4sTkMOzkKKh32Tme-LgN", "1vgzrnRUITFp_YYDztSlSgAioqRlSvUZ-"}');
